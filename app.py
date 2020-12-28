5a0e411ad4978f06a2175fcd600f5f6818ef531c
jguo <guojiayanc@gmail.com>
2020-12-27 16:01:32 -0800

    upload content to s3

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 from pytz import timezone
 import csv
 import io
+import boto3
 
 ARKK_HOLDINGS="https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"
 ARKQ_HOLDINGS="https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_AUTONOMOUS_TECHNOLOGY_&_ROBOTICS_ETF_ARKQ_HOLDINGS.csv"
@@ -22,20 +23,29 @@ arkholding = {
     "izrl": IZRL_HOLDINGS
 }
 
-# If the holding is not today, return false
+OBJECT_KEY_PATTERN="holdings/{today}_{etf}_holdings.csv"
+S3_BUCKET ="ark-fly"
+
+# If the holding date is not today, return false
 def get_etf_holdings(etf, date):
     response = requests.get(arkholding[etf])
     holding_date = get_holding_date(io.StringIO(response.text))
-    today = get_date()
-    if (holding_date == today):
-        fileName = today + "_" + etf + "_holdings.csv"
-        with open(fileName, 'wb') as temp_file: 
-            temp_file.write(response.content)
+    if (holding_date == date):
+        # fileName = today + "_" + etf + "_holdings.csv"
+        # with open(fileName, 'wb') as temp_file: 
+        #     temp_file.write(response.content)
+        upload_to_s3(io.BytesIO(response.content), OBJECT_KEY_PATTERN.format(today=date, etf=etf))
         return True
     else:
-        print("no need to ingest! holding_date {holding_date} today {today}".format(holding_date=holding_date, today=today))
+        print("no need to ingest! holding_date {holding_date} today {today}".format(holding_date=holding_date, today=date))
         return False
-    
+
+def upload_to_s3(content, object_name):
+    client = boto3.client('s3')
+    try:
+        response = client.upload_fileobj(content, S3_BUCKET, object_name)
+    except Exception as error:
+        raise Exception("faile to upload to s3! " + str(error))
 
 def get_date():
     tz = timezone('EST')
@@ -43,16 +53,22 @@ def get_date():
     return today
 
 def main():
+    today = get_date()
     for etf in arkholding:
-        today = get_date()
-        if(not get_etf_holdings(etf, today)):
+        if not get_etf_holdings(etf, today):
             return
 
 def get_holding_date(csv_bin):
     reader = csv.DictReader(csv_bin)
     for row in reader:
-        if (row["date"]):
+        if row["date"]:
             return datetime.strptime(row["date"], '%m/%d/%Y').strftime('%Y-%m-%d')
 
 if __name__ == '__main__':
     main()
+
+def lambda_handler(event, context):
+    main()
+    return {
+        "status":200
+    }
