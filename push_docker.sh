b065896d9ab3e16745474391bfaee07c0a5a9bbb
jguo <guojiayanc@gmail.com>
2020-12-27 16:30:39 -0800

    fix docker script

--- push_docker.sh
+++ push_docker.sh
@@ -2,6 +2,6 @@
 set -e
 
 docker build . -t ark-fly-ingest
-docker tag ark-go-email:latest 120400168286.dkr.ecr.us-west-2.amazonaws.com/ark-fly-ingest:latest
+docker tag ark-fly-ingest:latest 120400168286.dkr.ecr.us-west-2.amazonaws.com/ark-fly-ingest:latest
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 120400168286.dkr.ecr.us-west-2.amazonaws.com
 docker push 120400168286.dkr.ecr.us-west-2.amazonaws.com/ark-fly-ingest:latest
